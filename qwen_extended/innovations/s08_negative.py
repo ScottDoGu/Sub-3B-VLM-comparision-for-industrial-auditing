43fbd29d330c4a5851c23e1892be0941d1de4996
Scott Clayton <ledscottc@yahoo.com>
2026-04-26 19:30:34 -0500

    Implement Negative Constraint Prompting strategy class

---
+++ qwen_extended/innovations/s08_negative.py
@@ -0,0 +1,7 @@
+from qwen_extended.innovations.base import Strategy, base_prompt
+class NegativePrompting(Strategy):
+    name = "negative_prompting"
+    def run(self, engine, img, cat):
+        neg = "GUARDRAILS: Do NOT confuse reflections for needle." if "gauge" in cat else "GUARDRAILS: Do NOT confuse paint for corrosion."
+        r = engine.infer(base_prompt(cat) + neg + "Verdict PASS/FAIL, conf %.", images=[img])
+        return self._result(r["text"])
