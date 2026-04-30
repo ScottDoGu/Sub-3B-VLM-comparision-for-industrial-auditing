e234ef4385728871079d03fa3f7dc0ba866bca3b
Scott Clayton <ledscottc@yahoo.com>
2026-04-30 14:01:42 -0500

    Update registry.py

--- exhaustive_strategies/innovations/registry.py
+++ exhaustive_strategies/innovations/registry.py
@@ -1,19 +1,19 @@
-from .innovations.s1_baseline import Baseline
-from .innovations.s2_cot import ChainOfThought
-from .innovations.s3_decomposition import Decomposition
-from .innovations.s4_contrast import Contrast
-from .innovations.s5_contrast_cot import ContrastCoT
-from .innovations.s6_vision_only import VisionOnly
-from .innovations.s7_noise_injection import NoiseInjectedPrompt
-from .innovations.s8_divergence import DivergencePrompting
-from .innovations.s9_verification_only import VerificationOnly
-from .innovations.s10_structured_output import StructuredOutput
-from .innovations.s11_few_shot import FewShotGrounding
-from .innovations.s12_adaptive_router import AdaptiveEnsembleRouter
-from .innovations.s13_cross_model import MultiModelCrossCheck
-from .innovations.s14_multi_pass import MultiPassVerification
-from .innovations.s15_negative_evidence import NegativeEvidence
-from .innovations.s16_self_consistency import SelfConsistency
+from s1_baseline import Baseline
+from s2_cot import ChainOfThought
+from s3_decomposition import Decomposition
+from s4_contrast import Contrast
+from s5_contrast_cot import ContrastCoT
+from s6_vision_only import VisionOnly
+from s7_noise_injection import NoiseInjectedPrompt
+from s8_divergence import DivergencePrompting
+from s9_verification_only import VerificationOnly
+from s10_structured_output import StructuredOutput
+from s11_few_shot import FewShotGrounding
+from s12_adaptive_router import AdaptiveEnsembleRouter
+from s13_cross_model import MultiModelCrossCheck
+from s14_multi_pass import MultiPassVerification
+from s15_negative_evidence import NegativeEvidence
+from s16_self_consistency import SelfConsistency
 
 STRATEGY_REGISTRY = {
     "baseline": Baseline,
