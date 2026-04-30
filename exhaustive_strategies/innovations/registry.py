fdfbdd6c4c2f9c820ffde1ae37cd3dddf50f0568
Scott Clayton <ledscottc@yahoo.com>
2026-04-30 16:54:58 -0500

    Refactor strategy imports and update registry

--- exhaustive_strategies/innovations/registry.py
+++ exhaustive_strategies/innovations/registry.py
@@ -1,34 +1,35 @@
-from s1_baseline import Baseline
-from s2_cot import ChainOfThought
-from s3_decomposition import Decomposition
-from s4_contrast import Contrast
-from s5_contrast_cot import ContrastCoT
-from s6_vision_only import VisionOnly
-from s7_noise_injection import NoiseInjectedPrompt
-from s8_divergence import DivergencePrompting
-from s9_verification_only import VerificationOnly
-from s10_structured_output import StructuredOutput
-from s11_few_shot import FewShotGrounding
-from s12_adaptive_router import AdaptiveEnsembleRouter
-from s13_cross_model import MultiModelCrossCheck
-from s14_multi_pass import MultiPassVerification
-from s15_negative_evidence import NegativeEvidence
-from s16_self_consistency import SelfConsistency
+
+from .s01_clahe import CLAHEContrast
+from .s02_cot import ChainOfThought
+from .s03_contrast_cot import ContrastCoT
+from .s04_decomposition import TaskDecomposition
+from .s05_patches import MultiScalePatch
+from .s06_dual_pass import DualPassVerification
+from .s07_calibration import ConfidenceCalibration
+from .s08_negative import NegativePrompting
+from .s09_roi import ROICropping
+from .s10_structured import StructuredOutput
+from .s11_few_shot import FewShotGrounding
+from .s12_ensemble import AdaptiveEnsembleRouter
+from .s13_grid import GridOverlay
+from .s14_multipass_verification import MultiPassVerification
+from .s15_negative_evidence import NegativeEvidence
+from .s16_self_consistency import SelfConsistency
 
 STRATEGY_REGISTRY = {
-    "baseline": Baseline,
-    "cot": ChainOfThought,
-    "decomposition": Decomposition,
-    "contrast": Contrast,
+    "clahe_contrast": CLAHEContrast,
+    "chain_of_thought": ChainOfThought,
     "contrast_cot": ContrastCoT,
-    "vision_only": VisionOnly,
-    "noise_injection": NoiseInjectedPrompt,
-    "divergence": DivergencePrompting,
-    "verification_only": VerificationOnly,
+    "task_decomposition": TaskDecomposition,
+    "multi_scale_patch": MultiScalePatch,
+    "dual_pass_verification": DualPassVerification,
+    "confidence_calibration": ConfidenceCalibration,
+    "negative_prompting": NegativePrompting,
+    "roi_cropping": ROICropping,
     "structured_output": StructuredOutput,
     "few_shot_grounding": FewShotGrounding,
     "adaptive_ensemble_router": AdaptiveEnsembleRouter,
-    "cross_model_check": MultiModelCrossCheck,
+    "grid_overlay":GridOverlay,
     "multi_pass_verification": MultiPassVerification,
     "negative_evidence": NegativeEvidence,
     "self_consistency": SelfConsistency,
