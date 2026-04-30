b8e64953b83d534e1027b623bc9ed2f55b12d8fb
Scott Clayton <ledscottc@yahoo.com>
2026-04-30 13:18:56 -0500

    Refactor strategy imports and update registry

--- exhaustive_strategies/innovations/registry.py
+++ exhaustive_strategies/innovations/registry.py
@@ -1,22 +1,36 @@
-from exhaustive_strategies.innovations.s01_clahe import CLAHEContrast
-from exhaustive_strategies.innovations.s02_cot import ChainOfThought
-from exhaustive_strategies.innovations.s03_contrast_cot import ContrastCoT
-from exhaustive_strategies.innovations.s04_decomposition import TaskDecomposition
-from exhaustive_strategies.innovations.s05_patches import MultiScalePatch
-from exhaustive_strategies.innovations.s06_dual_pass import DualPassVerification
-from exhaustive_strategies.innovations.s07_calibration import ConfidenceCalibration
-from exhaustive_strategies.innovations.s08_negative import NegativePrompting
-from exhaustive_strategies.innovations.s09_roi import ROICropping
-from exhaustive_strategies.innovations.s10_structured import StructuredOutput
-from exhaustive_strategies.innovations.s11_few_shot import FewShotGrounding
-from exhaustive_strategies.innovations.s12_ensemble import AdaptiveEnsembleRouter
-from exhaustive_strategies.innovations.s13_grid import GridOverlay
+from .innovations.s1_baseline import Baseline
+from .innovations.s2_cot import ChainOfThought
+from .innovations.s3_decomposition import Decomposition
+from .innovations.s4_contrast import Contrast
+from .innovations.s5_contrast_cot import ContrastCoT
+from .innovations.s6_vision_only import VisionOnly
+from .innovations.s7_noise_injection import NoiseInjectedPrompt
+from .innovations.s8_divergence import DivergencePrompting
+from .innovations.s9_verification_only import VerificationOnly
+from .innovations.s10_structured_output import StructuredOutput
+from .innovations.s11_few_shot import FewShotGrounding
+from .innovations.s12_adaptive_router import AdaptiveEnsembleRouter
+from .innovations.s13_cross_model import MultiModelCrossCheck
+from .innovations.s14_multi_pass import MultiPassVerification
+from .innovations.s15_negative_evidence import NegativeEvidence
+from .innovations.s16_self_consistency import SelfConsistency
 
 STRATEGY_REGISTRY = {
-    "clahe_contrast": CLAHEContrast, "chain_of_thought": ChainOfThought, "contrast_cot": ContrastCoT,
-    "task_decomposition": TaskDecomposition, "multi_scale_patch": MultiScalePatch,
-    "dual_pass_verification": DualPassVerification, "confidence_calibration": ConfidenceCalibration,
-    "negative_prompting": NegativePrompting, "roi_cropping": ROICropping,
-    "structured_output": StructuredOutput, "few_shot_grounding": FewShotGrounding,
-    "adaptive_ensemble_router": AdaptiveEnsembleRouter, "grid_overlay": GridOverlay,
+    "baseline": Baseline,
+    "cot": ChainOfThought,
+    "decomposition": Decomposition,
+    "contrast": Contrast,
+    "contrast_cot": ContrastCoT,
+    "vision_only": VisionOnly,
+    "noise_injection": NoiseInjectedPrompt,
+    "divergence": DivergencePrompting,
+    "verification_only": VerificationOnly,
+    "structured_output": StructuredOutput,
+    "few_shot_grounding": FewShotGrounding,
+    "adaptive_ensemble_router": AdaptiveEnsembleRouter,
+    "cross_model_check": MultiModelCrossCheck,
+    "multi_pass_verification": MultiPassVerification,
+    "negative_evidence": NegativeEvidence,
+    "self_consistency": SelfConsistency,
 }
+
