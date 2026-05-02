bed8da81e8ce7d57363c1a302fe94d31a4ce4f53
Scott Clayton <ledscottc@yahoo.com>
2026-05-01 22:13:32 -0500

    Update registry.py

--- exhaustive_strategies/innovations/registry.py
+++ exhaustive_strategies/innovations/registry.py
@@ -1,4 +1,3 @@
-
 from .s01_clahe import CLAHEContrast
 from .s02_cot import ChainOfThought
 from .s03_contrast_cot import ContrastCoT
@@ -16,22 +15,22 @@ from .s14_multipass_verification import MultiPassVerification
 from .s15_negative_evidence import NegativeEvidence
 from .s16_self_consistency import SelfConsistency
 
-STRATEGY_REGISTRY = {
-    "clahe_contrast": CLAHEContrast,
-    "chain_of_thought": ChainOfThought,
-    "contrast_cot": ContrastCoT,
-    "task_decomposition": TaskDecomposition,
-    "multi_scale_patch": MultiScalePatch,
-    "dual_pass_verification": DualPassVerification,
-    "confidence_calibration": ConfidenceCalibration,
-    "negative_prompting": NegativePrompting,
-    "roi_cropping": ROICropping,
-    "structured_output": StructuredOutput,
-    "few_shot_grounding": FewShotGrounding,
-    "adaptive_ensemble_router": AdaptiveEnsembleRouter,
-    "grid_overlay":GridOverlay,
-    "multi_pass_verification": MultiPassVerification,
-    "negative_evidence": NegativeEvidence,
-    "self_consistency": SelfConsistency,
-}
-
+def get_all_strategies():
+    return {
+        "clahe_contrast": CLAHEContrast(),
+        "chain_of_thought": ChainOfThought(),
+        "contrast_cot": ContrastCoT(),
+        "task_decomposition": TaskDecomposition(),
+        "multi_scale_patch": MultiScalePatch(),
+        "dual_pass_verification": DualPassVerification(),
+        "confidence_calibration": ConfidenceCalibration(),
+        "negative_prompting": NegativePrompting(),
+        "roi_cropping": ROICropping(),
+        "structured_output": StructuredOutput(),
+        "few_shot_grounding": FewShotGrounding(),
+        "adaptive_ensemble": AdaptiveEnsembleRouter(),
+        "grid_overlay": GridOverlay(),
+        "multipass_verification": MultiPassVerification(),
+        "negative_evidence": NegativeEvidence(),
+        "self_consistency": SelfConsistency()
+    }
